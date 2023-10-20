ba1ec18ca05a6bf82d33a40aa6a02e8eb650a28e
mitaka3 <justinbituin1207@gmail.com>
2023-10-20 23:51:08 +0800

    initial commit

---
+++ Tablechairs.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class TableChairs 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("X                    X");
+        System.out.println("X                    X");
+        System.out.println("X     XXXXXXXXXX     X");
+        System.out.println("XXXXX X        X XXXXX");
+        System.out.println("X   X X        X X   X");
+        System.out.println("X   X X        X X   X");
+    }
+}
